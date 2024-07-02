40900dda06aae0f0d4fc878b437fe6d5b871a0a2
MD ABRAR LABIB <145818625+ASHASchowdhury@users.noreply.github.com>
2024-07-03 01:12:06 +0600

    Create phitron assignment -3b.cpp

---
+++ Hackerrank/phitron assignment -3b.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int n;
+cin>>n;
+vector<long long> a(n);
+
+for(int i=0;i<n;i++){
+    cin>>a[i];
+}
+vector<long long> b(n);
+b[0]=a[0];
+for(int i=1;i<n;i++){
+    b[i]=a[i]+b[i-1];
+}
+for(int i=n-1;i>=0;i--){
+cout<<b[i]<<" ";
+}
+return 0;
+}
