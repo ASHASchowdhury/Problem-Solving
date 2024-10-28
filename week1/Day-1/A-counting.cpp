4ea5895d5d480cd1a2b5019fcb8202298a46d5d4
MD ABRAR LABIB <145818625+ASHASchowdhury@users.noreply.github.com>
2024-10-28 23:51:17 +0600

    Create A-counting.cpp

---
+++ week1/Day-1/A-counting.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(a>b){
+        cout<<0<<endl;
+    }
+    else{
+        int x=(b-a)+1;
+        cout<<x<<endl;
+    }
+}
